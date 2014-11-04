5d8abefd62cd84f7a1543792a4f5b801a462fa62
age <jhswx84@gmail.com>
2014-11-04 17:48:10 +0800

    增加BlackArch

--- mirror_sync/rsync.blackarch.sh
+++ mirror_sync/rsync.blackarch.sh
@@ -1,11 +1,11 @@
 set -x
+#BlackArch同步脚本
+#从华中科技大学镜像站同步
 #SRC="rsync://mirror.umd.edu/archassault/"
-SRC="rsync://mirror.catn.com/pub/archassault/"
-#SRC="rsync://ftp.heanet.ie/pub/archassault/"
-#SRC="rsync://noodle.portalus.net/ArchAssault/"
-#SRC="rsync://mirror.jmu.edu/archassault/"
+#SRC="rsync://blackarch@blackarch.org/blackarch/blackarch/"
+SRC="rsync://mirrors.hustunique.com/blackarch/blackarch/"
 EXCLUDE_FILE="/media/repo/exclude.txt"
-#DST="/media/repo/ArchAssault/"
-DST="/media/cd1/ArchAssault/"
+DST="/media/repo/BlackArch/"
+#DST="/media/cd1/ArchAssault/"
 #rsync -avh --progress --partial --delete-after rsync://ftp.halifax.rwth-aachen.de/manjaro/ /media/repo/manjaro/
 rsync -avh --progress --partial --delete-after --exclude-from=$EXCLUDE_FILE $SRC $DST
